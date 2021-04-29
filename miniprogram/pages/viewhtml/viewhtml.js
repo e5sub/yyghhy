1b8965b0ec58c6f591a83f7aadcf0a06c186f618
yingyingguai <tanghui@fsmeeting.org>
2021-04-29 23:27:05 +0800

    同步酱茄V1.4.0代码

--- miniprogram/pages/viewhtml/viewhtml.js
+++ miniprogram/pages/viewhtml/viewhtml.js
@@ -1,11 +1,11 @@
 /*
- * 酱茄小程序开源版 v1.3.5
+ * 酱茄小程序开源版 v1.4.0
  * Author: 酱茄
  * Help document: https://www.jiangqie.com/ky
  * github: https://github.com/longwenjunjie/jiangqie_kafei
  * gitee: https://gitee.com/longwenjunj/jiangqie_kafei
  * License：MIT
- * Copyright ️ 2020 www.jiangqie.com All rights reserved.
+ * Copyright © 2020-2021 www.jiangqie.com All rights reserved.
  */
 
 const Api = require('../../utils/api.js');
@@ -18,11 +18,17 @@ Page({
 
     },
 
+    page_id: '',
+    title: '',
+
     onLoad: function (options) {
         let that = this;
         Rest.get(Api.JIANGQIE_POST_PAGE, {
             page_id: options.page_id
         }).then(res => {
+            that.page_id = options.page_id;
+            that.title = res.data.title;
+
             wx.setNavigationBarTitle({
                 title: res.data.title,
             })
@@ -33,14 +39,15 @@ Page({
 
     onShareAppMessage: function () {
         return {
-            title: getApp().appName,
-            path: 'pages/index/index',
+            title: this.title,
+            path: 'pages/viewhtml/viewhtml?page_id=' + this.page_id,
         }
     },
 
     onShareTimeline: function () {
         return {
-            title: getApp().appName,
+            title: this.title,
+            query: 'page_id=' + this.page_id
         }
     },
 
